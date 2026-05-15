268aab9d76675ebb71291ffd0e78bcc46ef37295
Ahmed Hussein <149941125+ahmed54047@users.noreply.github.com>
2026-05-15 12:56:09 +0300

    Change fetch URL to get random quote

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let containerquotes = document.querySelector('.container');
 
-fetch('https://dummyjson.com/quotes?limit=1')
+fetch('https://dummyjson.com/quotes/random')
   .then((res) => res.json())
   .then((data) => {
     containerquotes.innerHTML = '';
