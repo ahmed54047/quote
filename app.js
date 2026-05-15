bb7feab676ad58a8bc0b88c42ddadde458e8b143
Ahmed Hussein <149941125+ahmed54047@users.noreply.github.com>
2026-05-15 12:46:49 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+let containerquotes = document.querySelector('.container');
+
+fetch('https://dummyjson.com/quotes?limit=90')
+  .then((res) => res.json())
+  .then((data) => {
+    containerquotes.innerHTML = '';
+    data.quotes.forEach((quote) => {
+      // > انا بستخد الforeach  مع object     ,  اما لو  arry هستخدم map
+      containerquotes.innerHTML += `
+        <div class="quots">
+          <h2>${quote.quote}</h2>
+          <p>${quote.author}</p>
+        </div> 
+        <button> copy </button>
+      `;
+    });
+  });
